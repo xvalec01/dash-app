1340b24a48abb2f566e283c569ea968eba8a1e72
david_valecky_pekat <david.valecky@pekatvision.com>
2024-10-22 00:41:10 +0200

    feat: eal line chart added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
@@ -10,7 +11,7 @@ from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
 
-CC_DATASET = Path("./cc_dataset_processed.json")
+CC_DATASET = Path("D:\diplomka\seccerts-data\cc\processed_dataset.json")
 
 # Initialize Dash app
 app = Dash(__name__)
@@ -77,6 +78,8 @@ app.layout = html.Div(
         # Bar chart for category distribution per year
         dcc.Graph(id="category-year-bar-chart"),
         dcc.Graph(id="certificate-validity-boxplot"),
+        html.H1(children="Evolution of Average EAL Over Time"),
+        dcc.Graph(id="eal-line-chart"),
     ]
 )
 
@@ -181,6 +184,101 @@ def update_boxplot(selected_year):
         },
     )
     return figure
+
+
+def prepare_eal_data(df: pd.DataFrame) -> pd.DataFrame:
+    long_categories = {
+        "Access Control Devices and Systems": "Access control",
+        "Biometric Systems and Devices": "Biometrics",
+        "Boundary Protection Devices and Systems": "Boundary protection",
+        "ICs, Smart Cards and Smart Card-Related Devices and Systems": "ICs, Smartcards",
+        "Network and Network-Related Devices and Systems": "Network(-related) devices",
+    }
+    # Map EAL categories to numeric values
+    eal_to_num_mapping = {
+        eal: index
+        for index, eal in enumerate(df["eal"].astype(dtype="category").cat.categories)
+    }
+
+    df["eal_number"] = df.eal.map(eal_to_num_mapping)
+    df.eal_number = df.eal_number.astype("Int64")
+
+    # First avg_levels DataFrame grouped by 'year_from'
+    avg_levels = (
+        df.loc[(df.year_from < 2024) & (df.eal_number.notnull())]
+        .copy()
+        .groupby(["year_from"])
+        .agg({"year_from": "size", "eal_number": "mean"})
+        .rename(columns={"year_from": "n_certs"})
+        .reset_index()
+    )
+    avg_levels.year_from = avg_levels.year_from.astype("float")
+    avg_levels.eal_number = avg_levels.eal_number.astype("float")
+
+    # Second avg_levels DataFrame grouped by 'year_from' and 'category'
+    avg_levels = (
+        df.loc[df.eal_number.notnull()]
+        .copy()
+        .groupby(["year_from", "category"])
+        .agg({"year_from": "size", "eal_number": "mean"})
+        .rename(columns={"year_from": "n_certs"})
+        .reset_index()
+    )
+    avg_levels.year_from = avg_levels.year_from.astype("float")
+    avg_levels.eal_number = avg_levels.eal_number.astype("float")
+    avg_levels.category = avg_levels.category.map(lambda x: long_categories.get(x, x))
+
+    # Map categories to 'smartcard_category'
+    avg_levels["smartcard_category"] = avg_levels.category.map(
+        lambda x: x if x == "ICs, Smartcards" else "Other 14 categories"
+    )
+    return avg_levels, eal_to_num_mapping
+
+
+# Callback to update the line chart
+@app.callback(Output("eal-line-chart", "figure"), [Input("eal-line-chart", "id")])
+def update_line_chart(_):
+    # Create the figure for the line plot
+    df = fetch_data(CC_DATASET)
+    avg_levels, eal_to_num_mapping = prepare_eal_data(df)
+
+    fig = go.Figure()
+
+    # Separate data for each smartcard category
+    for category in avg_levels["smartcard_category"].unique():
+        category_data = avg_levels[avg_levels["smartcard_category"] == category]
+        fig.add_trace(
+            go.Scatter(
+                x=category_data["year_from"],
+                y=category_data["eal_number"],
+                mode="lines+markers",
+                name=category,
+                marker=dict(symbol="circle"),
+                line_shape="linear",
+            )
+        )
+
+    ymin = 1
+    ymax = 9
+    ylabels = [
+        x if "+" in x else x for x in list(eal_to_num_mapping.keys())[ymin : ymax + 1]
+    ]
+    # Set up layout configurations
+    fig.update_layout(
+        title="Average EAL Over Time for Smartcards and Other Categories",
+        xaxis_title="Year",
+        yaxis_title="Average EAL Number",
+        legend_title="Smartcard Category",
+        xaxis=dict(tickmode="linear", tick0=1998, dtick=1),  # Set x-axis ticks manually
+        yaxis=dict(
+            tickvals=np.arange(1, 10, 1), ticktext=ylabels
+        ),  # Set y-axis limits and ticks
+        margin=dict(t=20, l=20, r=20, b=20),  # Tight layout
+        width=1800,
+        height=800,
+    )
+
+    return fig
 
 
 # Run the Dash app
